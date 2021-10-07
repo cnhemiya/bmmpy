67080dc05b6361c5ae9d301d98f9b17f99534c6a
cnhemiya <cnhemiya@qq.com>
2021-10-07 12:19:35 +0800

    修改 replaceTextByDict 为 replaceStringByDict

--- src/jypack/jystring.py
+++ src/jypack/jystring.py
@@ -1,12 +1,9 @@
 # -*- coding: utf-8 -*-
 
 
-def replaceTextByDict(text, strDict):
-    """根据strDict提供的字符串字典替换text文本，
-    strDict={ "text_old1":"text_new1", "text_old2":"text_new2"}"""
-    result = ""
-    if (text != None) and (strDict != None):
-        for key in strDict:
-            result = text.replace(key, strDict[key])
-            text = result
-    return result
+def replaceStringByDict(string_, strDict):
+    """根据 strDict 提供的字符串字典替换 string_ 文本，
+    strDict={ "str_old1":"str_new1", "str_old2":"str_new2"}"""
+    for key in strDict:
+        string_ = string_.replace(key, strDict[key])
+    return string_
