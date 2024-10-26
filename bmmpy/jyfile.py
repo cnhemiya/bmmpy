879b8d4893b1122a90da82378c0847302cbe169d
cnhemiya <cnhemiya@qq.com>
2024-10-26 18:50:04 +0800

    合并文件 jydir 到 jyfile

--- bmmpy/jyfile.py
+++ bmmpy/jyfile.py
@@ -1,5 +1,7 @@
 # -*- coding: utf-8 -*-
 
+import os
+import glob
 import jypy.jystring
 
 
@@ -24,3 +26,29 @@ def writeText(file, text, encoding = "utf-8"):
     f = open(file = file, mode = "wt", encoding = encoding)
     f.write(text)
     f.close()
+
+
+def findFiles(dirName):
+    """用通配符查找dirName目录中的文件，dirName 例如 c:/*"""
+    fileList = []
+    for fileName in glob.glob(dirName):
+        if os.path.isfile(fileName):
+            fileList.append(fileName)
+    return fileList
+
+
+def execCmdInFiles(cmd, dirName):
+    """查找dirName目录中的文件并执行cmd命令，dirName 例如 c:/*.zip"""
+    files = findFiles(dirName)
+    for f in files:
+        os.system(cmd + "\"" + f + "\"")
+
+
+def findSubDirs(dirName):
+    """查找dirName目录中的子文件夹"""
+    dirList = []
+    for d in glob.glob(dirName + "/*"):
+        if os.path.isdir(d):
+            dirList.append(d)
+    return dirList
+
