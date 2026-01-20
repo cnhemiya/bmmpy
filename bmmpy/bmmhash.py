171bb220f10e0687b9a501150ff4ac5697318de2
cnhemiya <cnhemiya@qq.com>
2026-01-21 02:30:58 +0800

    优化注释

--- bmmpy/bmmhash.py
+++ bmmpy/bmmhash.py
@@ -8,18 +8,30 @@ LICENSE  MulanPSL2
 """
 
 import hashlib
+from typeguard import typechecked
 
 
-def md5_string(text="", encoding="utf-8"):
+@typechecked
+def md5_string(text: str = "", encoding: str = "utf-8") -> str:
     """
-    计算字符串的MD5值
+    计算字符串的 MD5 值。
 
     Args:
-        text (str): 要计算MD5的字符串，默认为空字符串
-        encoding (str): 字符串的编码，默认为 "utf-8"
+        text (str): 要计算 MD5 的字符串，默认为空字符串。
+        encoding (str): 字符串的编码，默认为 "utf-8"。
 
     Returns:
-        str: 计算得到的MD5值的十六进制表示
+        str: 计算得到的 MD5 值的十六进制表示。
+
+    Raises:
+        TypeError: 当 text 不是字符串类型时抛出。
+        LookupError: 当 encoding 无效时抛出。
+
+    Examples:
+        >>> md5_string("hello")
+        '5d41402abc4b2a76b9719d911017c593'
+        >>> md5_string("", "utf-8")
+        'd41d8cd98f00b204e9800998ecf8427e'
     """
     md5 = hashlib.md5()
     md5.update(bytearray(text.encode(encoding=encoding)))
