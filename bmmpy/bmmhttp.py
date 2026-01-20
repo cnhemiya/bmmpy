c814a74c6fb0cfa300d5b4aa85bfc1aeb09724a4
cnhemiya <cnhemiya@qq.com>
2026-01-21 02:50:09 +0800

    优化注释，增加参数检查装饰器 @typechecked，优化代码

--- bmmpy/bmmhttp.py
+++ bmmpy/bmmhttp.py
@@ -7,11 +7,14 @@ LICENSE  MulanPSL2
 @brief 网络 http 相关
 """
 
+
 import urllib.request
 import re
+from typeguard import typechecked
 
 
-def get_data_by_re(url, re_str):
+@typechecked
+def get_data_by_re(url: str, re_str: str) -> list[str]:
     """
     从指定 URL 中根据给定的正则表达式提取数据
 
@@ -20,14 +23,19 @@ def get_data_by_re(url, re_str):
         re_str (str): 正则表达式，用于匹配数据
 
     Returns:
-        list: 匹配到的数据列表
+        list[str]: 匹配到的数据列表
+
+    Examples:
+        >>> get_data_by_re("https://example.com", r"\\d+")
+        ['123', '456']
     """
     html = urllib.request.urlopen(url, timeout=60).read().decode("utf-8")
     data = re.findall(re_str, html)
     return data
 
 
-def download_data(url, file_name):
+@typechecked
+def download_data(url: str, file_name: str) -> None:
     """
     从指定 URL 下载数据并保存到文件
 
@@ -38,7 +46,6 @@ def download_data(url, file_name):
     Returns:
         None
     """
-    fn = open(file_name, "wb+")
-    data = urllib.request.urlopen(url, timeout=60).read()
-    fn.write(data)
-    fn.close()
+    with open(file_name, "wb+") as fn:
+        data = urllib.request.urlopen(url, timeout=60).read()
+        fn.write(data)
